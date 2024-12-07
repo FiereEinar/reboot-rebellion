b6bf6500ca9bb240dbed062fdda72ada0cd6607f
Nick Mours <nickxylanmelloria@gmail.com>
2024-12-07 08:22:23 +0800

    added custom cursor

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,10 @@
 package main;
 
+import java.awt.Cursor;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Main {
@@ -11,6 +16,12 @@ public class Main {
 		window.add(gp);
 		
 		window.pack();
+		
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Image cursorImage = toolkit.getImage(Main.class.getResource("/guns/crosshair.png"));
+		Cursor customCursor = toolkit.createCustomCursor(cursorImage, new Point(16, 16), "Custom Cursor");
+		window.setCursor(customCursor);
+		
 		window.setResizable(false);
 		window.setLocationRelativeTo(null);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
